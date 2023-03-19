be4394513558204e915b730886af0422edcb36f4
Oksana <oxana.manchuk@mail.ru>
2023-03-19 23:23:11 +0100

    feat(assets):add icons/assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,7 +1,27 @@
 import { ReactComponent as HomeIcon } from "./icons/home.svg";
 import { ReactComponent as FireIcon } from "./icons/fire.svg";
-import { ReactComponent as BookMark } from "./icons/marks.svg";
-import { ReactComponent as Setting } from "./icons/setting.svg";
+import { ReactComponent as BookMarkIcon } from "./icons/marks.svg";
+import { ReactComponent as SettingIcon } from "./icons/setting.svg";
 import { ReactComponent as MainLogo } from "./icons/logo-pixema.svg";
+import { ReactComponent as ButtonInput } from "./icons/buttonInput.svg";
 
-export { HomeIcon, FireIcon, BookMark, Setting, MainLogo };
+import { ReactComponent as UserIcon } from "./icons/user-profile.svg";
+
+import { ReactComponent as ArrowRightIcon } from "./icons/arrow-right.svg";
+
+import { ReactComponent as ArrowDownIcon } from "./icons/arrow-down.svg";
+
+import { ReactComponent as CloseIcon } from "./icons/close-cross.svg";
+
+export {
+  HomeIcon,
+  FireIcon,
+  BookMarkIcon,
+  SettingIcon,
+  MainLogo,
+  ButtonInput,
+  UserIcon,
+  ArrowRightIcon,
+  ArrowDownIcon,
+  CloseIcon,
+};
