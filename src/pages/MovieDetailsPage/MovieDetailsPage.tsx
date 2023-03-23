30aa81f825c6a727cb534166a4e6dea27ae77292
Oksana <oxana.manchuk@mail.ru>
2023-03-23 23:39:07 +0100

    feat(pages):modify Trends&MovieDetails

--- src/pages/MovieDetailsPage/MovieDetailsPage.tsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.tsx
@@ -1,5 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "store";
+import { fetchMoviesDetails } from "store/features";
+import { getDetailsMovie } from "store/selectors";
 
 export const MovieDetailsPage = () => {
+  const dispatch = useAppDispatch();
+  const { details, isLoading, error } = useAppSelector(getDetailsMovie);
+  const { imdbID = "" } = useParams();
+
+  const {
+    title,
+    genre,
+    poster,
+    imdbRating,
+    runtime,
+    plot,
+    year,
+    released,
+    boxOffice,
+    country,
+    production,
+    actors,
+    director,
+    writer,
+  } = details;
+
+  useEffect(() => {
+    dispatch(fetchMoviesDetails(imdbID));
+  }, [dispatch, imdbID]);
+
   return <div>MovieDetailsPage</div>;
 };
--- src/pages/TrendsPage/TrendsPage.tsx
+++ src/pages/TrendsPage/TrendsPage.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+import { MovieList } from "components";
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "store";
+import { fetchMoviesTrends } from "store/features";
+import { getMoviesTrends } from "store/selectors";
+import { StyledTrends } from "./styles";
 
 export const TrendsPage = () => {
-  return <div>TrendsPage</div>;
+  const dispatch = useAppDispatch();
+  const { movies, isLoading, error } = useAppSelector(getMoviesTrends);
+
+  useEffect(() => {
+    dispatch(fetchMoviesTrends({ page: 1 }));
+  }, [dispatch]);
+
+  return (
+    <StyledTrends>
+      {isLoading && <div>isload....</div>}
+
+      {error && <span>{error}</span>}
+
+      {movies?.length > 0 && <MovieList movies={movies} />}
+    </StyledTrends>
+  );
 };
