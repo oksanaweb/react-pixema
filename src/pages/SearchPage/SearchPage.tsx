aa75914ff45de941b7a18b7effe2f8f1ad97f862
Oksana <oxana.manchuk@mail.ru>
2023-03-23 12:44:00 +0100

    feat(pages):add changes to SearchPage

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -1,5 +1,30 @@
-import React from "react";
+import { MovieList } from "components";
+import React, { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "store";
+import { fetchMoviesByParameter } from "store/features";
+import { getMoviesByFilters } from "store/selectors";
+import { StyledBoxSearch } from "./styles";
 
 export const SearchPage = () => {
-  return <div>SearchPage</div>;
+  const dispatch = useAppDispatch();
+
+  const { isLoading, parameters, movies, error } =
+    useAppSelector(getMoviesByFilters);
+
+  useEffect(() => {
+    if (parameters.s || parameters.y || parameters.type)
+      dispatch(fetchMoviesByParameter(parameters));
+  }, [parameters]);
+
+  return (
+    <div>
+      <StyledBoxSearch>
+        {isLoading && <div>isload....</div>}
+
+        {error && <span>{error}</span>}
+
+        {movies?.length > 0 && <MovieList movies={movies} />}
+      </StyledBoxSearch>
+    </div>
+  );
 };
