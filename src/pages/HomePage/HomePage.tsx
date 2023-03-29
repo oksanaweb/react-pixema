e8d72bc7ab29c6a589f83ff80362a7927ea57a5a
Oksana <oxana.manchuk@mail.ru>
2023-03-29 23:44:43 +0200

    feat(pages): modify Homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,16 +1,22 @@
 import { MovieList } from "components";
 import React, { useEffect } from "react";
 import { fetchMovies, useAppDispatch, useAppSelector } from "store";
+import { fetchNextPageMovies, nextMoviePage } from "store/features";
 import { getMovies } from "store/selectors";
-import { StyledBox } from "./styles";
+import { StyledBox, StyledButton } from "./styles";
 
 export const HomePage = () => {
   const dispatch = useAppDispatch();
-  const { isLoading, movies, error } = useAppSelector(getMovies);
+  const { isLoading, movies, error, page } = useAppSelector(getMovies);
 
   useEffect(() => {
     dispatch(fetchMovies({ page: 1 }));
   }, [dispatch]);
+
+  const handleMovies = () => {
+    dispatch(nextMoviePage(true));
+    dispatch(fetchNextPageMovies({ page }));
+  };
 
   return (
     <StyledBox>
@@ -19,6 +25,8 @@ export const HomePage = () => {
       {error && <span>{error}</span>}
 
       {movies?.length > 0 && <MovieList movies={movies} />}
+
+      <StyledButton onClick={handleMovies}>Button</StyledButton>
     </StyledBox>
   );
 };
--- src/pages/HomePage/styles.ts
+++ src/pages/HomePage/styles.ts
@@ -1,3 +1,5 @@
 import styled from "styled-components";
 
 export const StyledBox = styled.div``;
+
+export const StyledButton = styled.button``;
