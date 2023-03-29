7d3b8646a627cfb5edba5f3a947f4be4aedf25d7
Oksana <oxana.manchuk@mail.ru>
2023-03-29 23:41:50 +0200

    feat(assets):rename icons

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,19 +1,14 @@
-import { ReactComponent as HomeIcon } from "./icons/home.svg";
-import { ReactComponent as FireIcon } from "./icons/fire.svg";
-import { ReactComponent as BookMarkIcon } from "./icons/marks.svg";
-import { ReactComponent as SettingIcon } from "./icons/setting.svg";
+import { ReactComponent as HomeIcon } from "./icons/home-icon.svg";
+import { ReactComponent as FireIcon } from "./icons/fire-icon.svg";
+import { ReactComponent as BookMarkIcon } from "./icons/marks-icon.svg";
+import { ReactComponent as SettingIcon } from "./icons/setting-icon.svg";
 import { ReactComponent as MainLogo } from "./icons/logo-pixema.svg";
-import { ReactComponent as ButtonInput } from "./icons/buttonInput.svg";
-
-import { ReactComponent as UserIcon } from "./icons/user-profile.svg";
-
-import { ReactComponent as ArrowRightIcon } from "./icons/arrow-right.svg";
-
-import { ReactComponent as ArrowDownIcon } from "./icons/arrow-down.svg";
-
-import { ReactComponent as CloseIcon } from "./icons/close-cross.svg";
-
-import BackgroundImage from "./image/background-image.png";
+import { ReactComponent as InputIcon } from "./icons/input-icon.svg";
+import { ReactComponent as UserIcon } from "./icons/user-profile-icon.svg";
+import { ReactComponent as ArrowRightIcon } from "./icons/arrow-right-icon.svg";
+import { ReactComponent as ArrowDownIcon } from "./icons/arrow-down-icon.svg";
+import { ReactComponent as CloseIcon } from "./icons/close-cross-icon.svg";
+import BackgroundMovies from "./image/background-movies.png";
 
 export {
   HomeIcon,
@@ -21,10 +16,10 @@ export {
   BookMarkIcon,
   SettingIcon,
   MainLogo,
-  ButtonInput,
+  InputIcon,
   UserIcon,
   ArrowRightIcon,
   ArrowDownIcon,
   CloseIcon,
-  BackgroundImage,
+  BackgroundMovies,
 };
