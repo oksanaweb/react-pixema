fba14f8e1dde4962db0bcc5643f95ad5db251b30
Oksana <oxana.manchuk@mail.ru>
2023-03-31 23:51:37 +0200

    feat(utils):modify firebaseAuthErrors

--- src/utils/firebaseAuthError.ts
+++ src/utils/firebaseAuthError.ts
@@ -1,12 +1,21 @@
 export enum FirebaseErrorMessage {
   WEAK_PASSWORD = "password must be contain 5 characters",
   UNKNOWN_ERROR = "unknown error -please reload page",
+  WRONG_PASSWORD = "wrong password",
+  EMAIL_EXISTS = "email exist",
 }
 
 export const getFirebaseErrorMessage = (code: string): FirebaseErrorMessage => {
   switch (code) {
     case "auth/weak-password":
       return FirebaseErrorMessage.WEAK_PASSWORD;
+
+    case "auth/wrong-password":
+      return FirebaseErrorMessage.WRONG_PASSWORD;
+
+    case "auth/email-already-in-use":
+      return FirebaseErrorMessage.EMAIL_EXISTS;
+
     default:
       return FirebaseErrorMessage.UNKNOWN_ERROR;
   }
