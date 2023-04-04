24862d06777df5e40cbee521be143d38ec6952c8
Oksana <oxana.manchuk@mail.ru>
2023-04-04 23:52:00 +0200

    modify SettingPage

--- src/pages/SettingPage/SettingsPage.tsx
+++ src/pages/SettingPage/SettingsPage.tsx
@@ -1,10 +1,11 @@
 import { SettingForm } from "components";
 import React from "react";
+import { Navigate } from "react-router-dom";
+import { ROUTE } from "router";
+import { useAppSelector } from "store";
+import { getUserInfo } from "store/selectors";
 
 export const SettingsPage = () => {
-  return (
-    <div>
-      <SettingForm />
-    </div>
-  );
+  const { isAuth } = useAppSelector(getUserInfo);
+  return isAuth ? <SettingForm /> : <Navigate to={ROUTE.Sign_in} />;
 };
