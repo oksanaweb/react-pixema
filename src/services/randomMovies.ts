c39002eed92ad66200abe9ac2c46564b3ad69b80
Oksana <oxana.manchuk@mail.ru>
2023-04-27 21:16:35 +0200

    feat(assets):modify UserIcon

--- src/services/randomMovies.ts
+++ src/services/randomMovies.ts
@@ -1,17 +1,4 @@
-const randomMovies = [
-  "Batman",
-  "Wars",
-  "Dream",
-  "Marvel",
-  "Man",
-  "Adventure",
-  "Life",
-  "Potter",
-  "Hobbit",
-  "World",
-  "Dark",
-  "Sherlock",
-];
+const randomMovies = ["Batman", "Wars", "Marvel", "Man", "Adventure", "Life", "World", "Dark", "Sherlock"];
 
 export const getRandomMovie = () => {
   const randomMovie = randomMovies[(Math.random() * randomMovies.length) | 0];
